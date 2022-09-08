3cd30515c69124d1a46c6e6e096c8d70b9442a39
Tapan221 <tapanachary.44@gmail.com>
2022-09-09 00:21:41 +0530

    Test Base Update

--- src/main/java/com/ecomm/qa/base/TestBase.java
+++ src/main/java/com/ecomm/qa/base/TestBase.java
@@ -35,14 +35,7 @@ public class TestBase {
 
 		if (browserName.equals("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-			options.addArguments("start-maximized"); // open Browser in maximized mode
-			options.addArguments("disable-infobars"); // disabling infobars
-			options.addArguments("--disable-extensions"); // disabling extensions
-			options.addArguments("--disable-gpu"); // applicable to windows os only
-			options.addArguments("--no-sandbox"); // Bypass OS security model
-			driver = new ChromeDriver(options);
+			driver = new ChromeDriver();
 		} else if (browserName.equals("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
