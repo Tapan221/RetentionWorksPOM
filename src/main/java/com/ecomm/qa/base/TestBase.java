e935c0ec1716bf251b30d5e2a502832c891c5df4
Tapan221 <tapanachary.44@gmail.com>
2022-09-09 01:17:35 +0530

    Test Base Update

--- src/main/java/com/ecomm/qa/base/TestBase.java
+++ src/main/java/com/ecomm/qa/base/TestBase.java
@@ -36,13 +36,16 @@ public class TestBase {
 		if (browserName.equals("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			ChromeOptions options = new ChromeOptions();
+			options.addArguments("headless","no-sandbox");
 			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 			options.addArguments("start-maximized"); // open Browser in maximized mode
 			options.addArguments("disable-infobars"); // disabling infobars
 			options.addArguments("--disable-extensions"); // disabling extensions
 			options.addArguments("--disable-gpu"); // applicable to windows os only
-			options.addArguments("--no-sandbox"); // Bypass OS security model
+			 // Bypass OS security model
 			options.addArguments("--remote-debugging-port=9222");
+			options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
+			driver = new ChromeDriver(options);
 			
 			driver = new ChromeDriver(options);
 		} else if (browserName.equals("firefox")) {
